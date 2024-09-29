1ea9f351ef525e84dfa9a79a56da4d18e7f24c89
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-29 19:53:49 +0000

    chore: remove env

--- initVNC.sh
+++ initVNC.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 mkdir ~/docker/webtop/config -p
-
-echo "PASSWORD=Password_0" > ~/docker/webtop/.env
--- startVNC.sh
+++ startVNC.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --quiet -d --name webtop --env-file /home/$USER/docker/webtop/.env -e TZ=America/New_York -e PUID=1000 -e PGID=1000 -v /home/$USER/docker/webtop/config:/config --restart=unless-stopped --network=host linuxserver/webtop:fedora-kde
+docker run --quiet -d --name webtop -e TZ=America/New_York -e PUID=1000 -e PGID=1000 -v /home/$USER/docker/webtop/config:/config --restart=unless-stopped --network=host linuxserver/webtop:fedora-kde
