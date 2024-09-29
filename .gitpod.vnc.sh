2075552d79878b13cd7960d14fc1f2efe7fff411
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-29 21:12:55 +0000

    test: new image

--- .gitpod.vnc.sh
+++ .gitpod.vnc.sh
@@ -2,6 +2,6 @@
 
 mkdir ~/docker/webtop/config -p
 
-docker run --quiet -d --name webtop -p 6080:6080 -e TZ=America/New_York -e PUID=1000 -e PGID=1000 -v /home/$USER/docker/webtop/config:/config --restart=unless-stopped --network=host linuxserver/webtop:ubuntu-i3
+docker run --quiet -d --name webtop -p 6080:6080 -e TZ=America/New_York -e PUID=1000 -e PGID=1000 -v /home/$USER/docker/webtop/config:/config --restart=unless-stopped --network=host linuxserver/webtop:arch-kde
 
 docker exec webtop chromium-browser https://localhost:3000 --no-sandbox -test-type --fullscreen
